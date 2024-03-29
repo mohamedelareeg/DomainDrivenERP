c20194fa459188532b0213bd5d43a088239ce286
PC <PC@DESKTOP-6RI4GMT>
2024-03-30 01:08:53 +0200

    Resiliency with Polly

--- src/CleanArchitectureWithDDD.Persistence/BackgroundJobs/ProcessOutboxMessagesJob.cs
+++ src/CleanArchitectureWithDDD.Persistence/BackgroundJobs/ProcessOutboxMessagesJob.cs
@@ -3,6 +3,7 @@ using CleanArchitectureWithDDD.Persistence.OutBox;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Polly;
 using Quartz;
 using System;
 using System.Collections.Generic;
@@ -22,21 +23,41 @@ namespace CleanArchitectureWithDDD.Persistence.BackgroundJobs
             _context = context;
             _publisher = publisher;
         }
+        private static readonly JsonSerializerSettings jsonSerializerSettings = new()
+        {
+            TypeNameHandling = TypeNameHandling.All,
+        };
+        
         public async Task Execute(IJobExecutionContext context)
         {
             try
             {
-                var messages = await _context.Set<OutboxMessage>().Where(a => a.ProcessedOnUtc == null).Take(20).ToListAsync(context.CancellationToken);
+                var messages = await _context.Set<OutboxMessage>()
+                    .Where(a => a.ProcessedOnUtc == null)
+                    .Take(20)
+                    .ToListAsync(context.CancellationToken);
                 foreach (var message in messages)
                 {
-                    var domainEvent = JsonConvert.DeserializeObject<IDomainEvent>(message.Content,new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All});
+                    var domainEvent = JsonConvert.DeserializeObject<IDomainEvent>(
+                        message.Content,
+                        jsonSerializerSettings);
                     if (domainEvent is null)
                     {
                         //TODO : Logging
                         continue;
                     }
-                    await _publisher.Publish(domainEvent, context.CancellationToken);
+                    var policy = Policy
+                        .Handle<Exception>()
+                        .WaitAndRetryAsync(
+                            3, 
+                            attempt => TimeSpan.FromMicroseconds(50 * attempt));
+                    var result = await policy.ExecuteAndCaptureAsync(() => 
+                        _publisher.Publish(
+                            domainEvent,
+                            context.CancellationToken));
+                    message.Error = result.FinalException?.ToString();
                     message.ProcessedOnUtc = DateTime.UtcNow;
+
                 }
                 await _context.SaveChangesAsync();
             }
